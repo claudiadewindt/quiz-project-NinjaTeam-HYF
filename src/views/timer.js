b312e74d6ee4b04e0e6ea4e613d0effca0fa9934
Obada <obadamoustafa@gmail.com>
2021-08-07 13:21:32 +0200

    timer is working

---
+++ src/views/timer.js
@@ -0,0 +1,30 @@
+
+import { getDOMElement } from "../utils/DOMUtils.js";
+import { nextQuestion } from "../listeners/questionListeners.js";
+
+// Timer function
+
+export const timerEl = getDOMElement('timer');
+export let activateTimer;
+
+// activate timer function.
+export const activateTimerFn = ()=>{
+  timerEl.textContent = 10;
+  activateTimer = window.setInterval(counterTimer, 1000)
+}
+// deactivate timer function.
+export const deactivateTimerFn = ()=>{
+  clearInterval(activateTimer);
+}
+
+export let counterTimer = () => {
+    let timerNumber = parseFloat(timerEl.textContent);
+    if (timerNumber > 0) {
+        timerEl.textContent = timerNumber -1;
+        
+    } else {
+        // go to next question
+        deactivateTimerFn();
+        nextQuestion();
+    }
+}
